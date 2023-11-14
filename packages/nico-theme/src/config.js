94df153885d0a5841e4f3b4015af1b1453f18cfb
Marcus Mourão <marcusmmourao@gmail.com>
2023-11-14 10:33:15 -0300

    feat: generate tokens and exports

--- packages/nico-theme/src/config.js
+++ packages/nico-theme/src/config.js
@@ -1,18 +1,34 @@
-
-
 const StyleDictionary = require('style-dictionary').extend({
   source: [`${__dirname}/tokens/**/*.json`],
   platforms: {
     css: {
       transformGroup: 'css',
       buildPath: 'dist/theme/',
-      files: [{
-        destination: 'nico-theme__default.css',
-        format: 'css/variables'
-      }]
-    }
+      files: [
+        {
+          destination: 'nico-theme__default.css',
+          format: 'css/variables',
+        },
+      ],
+      options: {
+        selector: ".nico__theme--default"
+      }
+    },
+    ts: {
+      transformGroup: 'js',
+      files: [
+        {
+          format: 'javascript/module',
+          destination: 'dist/index.js',
+        },
+        {
+          format: 'typescript/module-declarations',
+          destination: 'dist/index.d.ts',
+        },
+      ],
+    },
     // ...
-  }
+  },
 });
 
 StyleDictionary.buildAllPlatforms();
